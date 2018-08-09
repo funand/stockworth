1b5853fdb587b53d90fc3bd95c272a19a79b2b95
Fogthecatman <fogthecatman@gmail.com>
2018-08-08 19:42:18 -0500

    React Base

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import DashboardPage from '../components/DashboardPage';
+import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+export const history = createHistory();
+
+
+const AppRouter = () => (
+    <Router history={history}>
+        <div>
+            <Switch>
+                <Route
+                    path="/"
+                    component={DashboardPage}
+                    exact={true}
+                />
+                <Route 
+                    path="/dashboard"
+                    component={DashboardPage}
+                />
+                <Route 
+                    component={NotFoundPage}
+                />
+            </Switch>
+        </div>
+    </Router>
+);
+
+export default AppRouter;
