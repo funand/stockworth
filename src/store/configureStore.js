8e7c0d7325ede36fe958f40aef429be8ed18c8cb
Prince Ikede <davidikede24@gmail.com>
2018-09-04 20:38:26 +0200

    Commited

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,8 +3,6 @@ import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// Store Creation
-
 export default () => {
     const store = createStore(
         combineReducers({}),
